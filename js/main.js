a7cdd6c56ee6e5f1f0bff840bfb6f14d8052764a
Damir <deka-1999@mail.ru>
2023-03-06 18:37:12 +0700

    Naming fixies

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ const COMMENTS_TEXT = [
 
 
 // Function creates random number from set range
-const GET_RANDOM_INTEGER = (a, b) => {
+const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
@@ -25,46 +25,46 @@ const GET_RANDOM_INTEGER = (a, b) => {
 };
 
 //Function return path URL yse to access PHOTOS (photo folder) by name that is number randomly generated
-const URL_GENERATOR_FOR_PHOTO = function (format) {
+const photoUrlGenerator = function (format) {
   return function (Number) {
     return `photos/${Number}.${format}`;
   };
 };
 
-const JPG_GENERATOR = URL_GENERATOR_FOR_PHOTO('jpg'); // As there are 25 photos existed
+const jpgGenerator = photoUrlGenerator('jpg'); // As there are 25 photos existed
 
 //Function return path URL yse to access PHOTOS (photo folder) by name that is number randomly generated
-const URL_GENERATOR_FOR_AVATAR = function (format) {
+const avatarUrlGenerator = function (format) {
   return function (Number) {
     return `img/avatar-${Number}.${format}`;
   };
 };
 
-const SVG_GENERATOR = URL_GENERATOR_FOR_AVATAR('svg');// As there are 6 avatar existed
+const svgGenerator = avatarUrlGenerator('svg');// As there are 6 avatar existed
 
 //Function creates an object with necessary data
 let currentNumber = 0;
 const getPhotoInfo = function () {
   currentNumber += 1;
-  const CURRENT_ID = currentNumber;
-  const RANDOM_NAME_INDEX = GET_RANDOM_INTEGER(0, AVATAR_NAMES.length - 1);
-  const RANDOM_COMMENT_INDEX = GET_RANDOM_INTEGER(0, COMMENTS_TEXT.length - 1);
+  const currentId = currentNumber;
+  const randomNameIndex = getRandomInteger(0, AVATAR_NAMES.length - 1);
+  const randomCommentIndex = getRandomInteger(0, COMMENTS_TEXT.length - 1);
 
   return {
-    id: CURRENT_ID,
-    url: JPG_GENERATOR(CURRENT_ID),
+    id: currentId,
+    url: jpgGenerator(currentId),
     description: 'Хорошее фото',
-    likes: GET_RANDOM_INTEGER(15, 200),
+    likes:getRandomInteger(15, 200),
     comments: {
-      id: GET_RANDOM_INTEGER(1, 1000),
-      avatar: SVG_GENERATOR(GET_RANDOM_INTEGER(1, 6)),
-      message: COMMENTS_TEXT[RANDOM_COMMENT_INDEX],
-      name: AVATAR_NAMES[RANDOM_NAME_INDEX],
+      id: getRandomInteger(1, 1000),
+      avatar: svgGenerator(getRandomInteger(1, 6)),
+      message: COMMENTS_TEXT[randomCommentIndex],
+      name: AVATAR_NAMES[randomNameIndex],
     },
   };
 };
 
 getPhotoInfo();
-// const SEVERAL_PHOTOS_DATA = Array.from({length: 25}, getPhotoInfo);
-//Можно вызывать SEVERAL_PHOTOS_DATA через console.log(SEVERAL_PHOTOS_DATA);
-// console.log(SEVERAL_PHOTOS_DATA);
+// const severaPhotosData = Array.from({length: 25}, getPhotoInfo);
+//Можно вызывать severaPhotosData через console.log(severaPhotosData);
+// console.log(severaPhotosData);
