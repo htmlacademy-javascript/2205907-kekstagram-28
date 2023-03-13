2f1bbfbba1532aa9a4d9c49301d55f0403d028a9
Keks <keks@htmlacademy.ru>
2023-03-13 13:35:17 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,5 @@
+import './data.js';
+import './util.js';
+
+//Для проверки корректности работы функции createPhotosData() , необходимо ее испортировать из файла ./data.js
+// console.log(createPhotosData());
